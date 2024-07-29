770d08991ada575e434c9d47f71a1ec1be7214e4
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-07-29 10:38:46 -0300

    arrancusi

---
+++ Tp-ToDo-Native/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Modal } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header>
+        <Text>To-Do List</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+      </Header>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 75,
+    padding: 10,
+    float: left,
+    fontSize: 30
+  },
+});
---
+++ Tp-ToDo-Native/components/Header.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, TextInput, Modal } from 'react-native'
+
+export default function Header() {
+    return (
+      <View style={styles.container}>
+        <Header>
+          <Text>To-Do List</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+          />
+        </Header>
+      </View>
+  
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      width: 75,
+      padding: 10,
+      float: left,
+      fontSize: 30
+    },
+  });
